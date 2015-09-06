4922b92c1750c6d580c8d56ab798d302fc3940b3
nIQnutn <sn@niqnutn.com>
2015-09-06 21:22:20 +0200

    Update fr-help.php

--- lang/fr-help.php
+++ lang/fr-help.php
@@ -1,49 +1,32 @@
-<h2>Informations</h2>
-<p>Ce plugin est un fork de celui réalisé par Websyys que je tiens à remercier pour son idée et pour les icônes des différents RS.</p>
-<p>En éditant ce plugin vous pourrez voir qu’il se compose en deux méthodes. La première permet l’ajout du css à PluXML et la deuxième permet d’afficher les liens des RS.</p>
-<br />
 <h2>Aide</h2>
-<p>L’affichage par défaut se présente comme ceci :</p>
-<p style="padding-left:40px"><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/plugins/aplxSocialImg/demo.png" alt="demo" /></p>
-<p>Pour obtenir ce rendu sur votre PluXML, ajoutez ce qui suit à votre fichier article.php ou dans la boucle d’affichage des articles de votre home.php :</p>
-<pre style="font-size:12px; padding-left:40px">&lt;?php eval($plxShow->callHook('SocialImg')) ?&gt;</pre>
-<br />
-<p>Ci-dessous, l’image par défaut permettant l’affichage des liens :
-<p style="padding-left:40px"><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/plugins/aplxSocialImg/img.png" alt="image RS" /></p>
-<p>Vous pouvez bien évidemment la télécharger, la modifier puis la stocker dans votre thème pour ensuite surcharger le css de ce plugin avec le vôtre en faisant comme ceci :</p>
-<pre style="font-size:12px; padding-left:40px">
-	.social li a {
-		background: url('<b>URL_IMG_ICI</b>') 0 0 no-repeat !important;
-	}
-</pre>
-<br />
-<p>Pour ceux qui voudraient aller plus loin, je donne ci-dessous le contenu du css du plugin, pensez à le surcharger avec le vôtre, soit en ajoutant !important à la fin de vos lignes :</p>
-<pre style="font-size:12px; padding-left:40px">
-	.social ul {
-		float: left;
-	}
 
-	.social li {
-		margin: 0px;
-		display: inline-block;
-	}
+Pour afficher les boutons des r&eacute;seaux sociaux Facebook, Twitter, Google+1, LinkedIn, Diaspora, Courriel
 
-	.social li a {
-		margin: 0px;
-		display: block;
-		background: url('<b>URL_IMG_ICI</b>') 0 0 no-repeat;
-		width: 29px;
-		height: 32px;
-	}
+<h3 style="font-size:1.3em;font-weight:bold;padding:10px 0 10px 0">Dans les articles</h3>
+<p>
+Dans le fichier <strong>article.php</strong> de votre thème, ajoutez la ligne suivante à l'endroit où vous souhaitez afficher les boutons.
+</p>
+<pre style="font-size:12px; padding-left:40px"><code>
+&lt;?php eval($plxShow->callHook('MySocialNetworkArticle')) ?&gt;
+</code></pre>
 
-	.social li a.comment		{	background-position: 0px 0px;		}
-	.social li a.comment:hover	{	background-position: 0px -32px;		}
-	.social li a.gplus		{	background-position: -32px 0px;		}
-	.social li a.gplus:hover	{	background-position: -32px -32px;	}
-	.social li a.facebook		{	background-position: -64px 0px;		}
-	.social li a.facebook:hover	{	background-position: -64px -32px;	}
-	.social li a.twitter		{	background-position: -96px 0px;		}
-	.social li a.twitter:hover	{	background-position: -96px -32px;	}
-	.social li a.linkedin		{	background-position: -128px 0px;	}
-	.social li a.linkedin:hover	{	background-position: -128px -32px;	}
-</pre>
+<h3 style="font-size:1.3em;font-weight:bold;padding:10px 0 10px 0">Dans les pages statiques</h3>
+<p>
+Dans le fichier <strong>static.php</strong> de votre thème, ajoutez la ligne suivante à l'endroit où vous souhaitez afficher les boutons.
+</p>
+<pre style="font-size:12px; padding-left:40px"><code>
+&lt;?php eval($plxShow->callHook('MySocialNetworkStatic')) ?&gt;
+</code></pre>
+
+<h3 style="font-size:1.3em;font-weight:bold;padding:10px 0 10px 0">Modification affichage des boutons</h3>
+<p>
+L'affichage des boutons peut être modifié, en personnalisant la classe <strong>.social-network </strong> en allant sur l'écran d'administration:
+<br /><br />
+Paramètres > Plugins > menu "Plugins actifs" > plugin "MySocialNetwork" > menu "Code css" > champ "Contenu fichier css site"
+</p>
+
+<p>
+Cliquez sur le bouton "Sauvegarder le fichier" pour enregistrer les modifications.
+</p>
+
+
