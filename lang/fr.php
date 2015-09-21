9eb77eb236b4b4a372af1829981ade149b608c00
nIQnutn <sn@niqnutn.com>
2015-09-21 23:18:52 +0200

    Ajout traduction

--- lang/fr.php
+++ lang/fr.php
@@ -1,5 +1,23 @@
 <?php
 	$LANG = array(
+
+# config.php
 		'L_PLUGIN_TITLE'	=>	'plxMySocialNetwork',
+		'L_MANAGE_PLUGIN'	=>	'Gestion du plugin MySocialNetwork',
+		'L_SOCIAL_NETWORK'  =>  'Réseaux sociaux',
+		'L_NAME'			=>  'Nom',
+		'L_ACTIVE'			=>	'Active',
+		'L_URL'				=>	'URL',
+		'L_DESCRIPTION'		=>	'Description',
+		'L_CLASS'			=>	'class',
+		'L_MAIL'			=>	'Courriel',
+		'L_MAIL_SUBJECT'	=>	'Sujet',
+		'L_MAIL_BODY'		=>	'Corps du message',
+		'L_HELP'			=> 	'Aide',
+		
+# plxMySocialNetwork
+		'L_SHARE_ARTICLE'			=>	'Partager l\'article: ',
+		'L_SHARE_STATIC'			=>	'Partager la page: '
+
 	);
 ?>
