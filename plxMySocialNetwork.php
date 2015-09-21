3cf1a80df249b79a9300cb72335a20bfed72ab10
nIQnutn <sn@niqnutn.com>
2015-09-21 23:28:21 +0200

    version 1.2

--- plxMySocialNetwork.php
+++ plxMySocialNetwork.php
@@ -3,7 +3,7 @@
  /**
  * Plugin plxMySocialNetwork by nIQnutn
  * Update: 21 september 2015
- * Version:	1.1
+ * Version:	1.2
  * 
  * Forked from aplxSocialImg by aruhuno on 06 september 2015
  **/
