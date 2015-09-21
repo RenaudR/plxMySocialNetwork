6f4138c57ea90cbe2d3910c8f4436583132b5c3a
nIQnutn <sn@niqnutn.com>
2015-09-21 23:19:44 +0200

    Ajout traduction

--- plxMySocialNetwork.php
+++ plxMySocialNetwork.php
@@ -1,35 +1,29 @@
 <?php
+
  /**
- * Plugin plxMySocialNetwork par nIQnutn
- *
- * D'après le plugin aplxSocialImg par aruhuno
+ * Plugin plxMySocialNetwork by nIQnutn
+ * Update: 21 september 2015
+ * Version:	1.1
  * 
- * Fork 06 september 2015
- * Update 21 september 2015
- *
+ * Forked from aplxSocialImg by aruhuno on 06 september 2015
  **/
+
+
 class plxMySocialNetwork extends plxPlugin {
 
-
 	/**
-	 * Constructeur de la classe
+	 * Constructeur de la classe plxMySocialNetwork
 	 *
-	 * @param	default_lang	langue par défaut
-	 * @return	stdio
-	 * @author	nIQnutn
+	 * @author	 nIQnutn
 	 **/
 	 
-	 
-	private $socialNetworkFile = "data/configuration/plugins/plxMySocialNetwork-data.xml";
-
-
 	public function __construct($default_lang) {
 
 		# appel du constructeur de la classe plxPlugin (obligatoire)
 		parent::__construct($default_lang);
 		
 		# limite l'accès à l'écran d'administration du plugin
-		$this-> setConfigProfil(PROFIL_ADMIN, PROFIL_MANAGER);		
+		$this-> setConfigProfil(PROFIL_ADMIN);		
 
 		# déclaration des hooks
 		$this->addHook('MySocialNetworkArticle', 'MySocialNetworkArticle');
@@ -38,91 +32,83 @@ class plxMySocialNetwork extends plxPlugin {
 
 
 	 /**
-	 * Méthode qui initialise les réseaux sociaux par défaut 
-	 *
+	 * Méthode utilisée à l'activation du plugin
+	 * initialise les réseaux sociaux par défaut 
+	 * 
 	 * @author	 nIQnutn
 	 **/
 	public function OnActivate() {
 		
-      			$this->setParam('a-param1', 'Twitter', 'cdata');
-      			$this->setParam('a-param2', '1', 'string');
-      			$this->setParam('a-param3', 'https://twitter.com/share?url={$url}&text={$title}', 'cdata');
-      			$this->setParam('a-param4', 'Partager sur Twitter', 'cdata');
-      			$this->setParam('a-param5', 'twitter', 'cdata');
-      			$this->setParam('a-param6', '1', 'numeric');
+			$this->setParam('mail-param1', 'Courriel', 'cdata');
+			$this->setParam('mail-param2', '1', 'string');
+			$this->setParam('mail-param3', 'mailto:?subject={$title}&amp;body={$title}:&nbsp;{$url}', 'cdata');
+			$this->setParam('mail-param4', 'Partager par courriel', 'cdata');
+			$this->setParam('mail-param5', 'courriel', 'cdata');
+			$this->setParam('mail-param9', '{$title}', 'cdata');
+			$this->setParam('mail-param0', '{$title}:&nbsp;{$url}', 'cdata');
+			
+			$this->setParam('a-param1', 'Twitter', 'cdata');
+			$this->setParam('a-param2', '1', 'string');
+			$this->setParam('a-param3', 'https://twitter.com/share?url={$url}&text={$title}', 'cdata');
+			$this->setParam('a-param4', 'Partager sur Twitter', 'cdata');
+			$this->setParam('a-param5', 'twitter', 'cdata');
 
-      			$this->setParam('b-param1', 'Facebook', 'cdata');
-      			$this->setParam('b-param2', '1', 'string');
-      			$this->setParam('b-param3', 'https://www.facebook.com/sharer/sharer.php?u={$url}&t={$title}', 'cdata');
-      			$this->setParam('b-param4', 'Partager sur Facebook', 'cdata');
-      			$this->setParam('b-param5', 'facebook', 'cdata');
-      			$this->setParam('b-param6', '2', 'numeric');
+			$this->setParam('b-param1', 'Facebook', 'cdata');
+			$this->setParam('b-param2', '1', 'string');
+			$this->setParam('b-param3', 'https://www.facebook.com/sharer/sharer.php?u={$url}&t={$title}', 'cdata');
+			$this->setParam('b-param4', 'Partager sur Facebook', 'cdata');
+			$this->setParam('b-param5', 'facebook', 'cdata');
 
-      			$this->setParam('c-param1', 'Google+', 'cdata');
-      			$this->setParam('c-param2', '1', 'string');
-      			$this->setParam('c-param3', 'https://plus.google.com/share?url={$url}', 'cdata');
-      			$this->setParam('c-param4', 'Partager sur Google+', 'cdata');
-      			$this->setParam('c-param5', 'google-plus', 'cdata');
-      			$this->setParam('c-param6', '3', 'numeric');
-      			
-      			$this->setParam('d-param1', 'LinkedIn', 'cdata');
-      			$this->setParam('d-param2', '1', 'string');
-      			$this->setParam('d-param3', 'https://www.linkedin.com/shareArticle?mini=true&amp;url={$url}&amp;title={$title}', 'cdata');
-      			$this->setParam('d-param4', 'Partager sur LinkedIn', 'cdata');
-      			$this->setParam('d-param5', 'linkedin', 'cdata');
-      			$this->setParam('d-param6', '4', 'numeric');
+			$this->setParam('c-param1', 'Google+', 'cdata');
+			$this->setParam('c-param2', '1', 'string');
+			$this->setParam('c-param3', 'https://plus.google.com/share?url={$url}', 'cdata');
+			$this->setParam('c-param4', 'Partager sur Google+', 'cdata');
+			$this->setParam('c-param5', 'google-plus', 'cdata');
+			
+			$this->setParam('d-param1', 'LinkedIn', 'cdata');
+			$this->setParam('d-param2', '1', 'string');
+			$this->setParam('d-param3', 'https://www.linkedin.com/shareArticle?mini=true&amp;url={$url}&amp;title={$title}', 'cdata');
+			$this->setParam('d-param4', 'Partager sur LinkedIn', 'cdata');
+			$this->setParam('d-param5', 'linkedin', 'cdata');
 
-      			$this->setParam('e-param1', 'Diaspora', 'cdata');
-      			$this->setParam('e-param2', '1', 'string');
-      			$this->setParam('e-param3', 'https://share.diasporafoundation.org/?title={$title}&amp;url={$url}', 'cdata');
-      			$this->setParam('e-param4', 'Partager sur Diaspora', 'cdata');
-      			$this->setParam('e-param5', 'diaspora', 'cdata');
-      			$this->setParam('e-param6', '5', 'numeric');
+			$this->setParam('e-param1', 'Diaspora', 'cdata');
+			$this->setParam('e-param2', '1', 'string');
+			$this->setParam('e-param3', 'https://share.diasporafoundation.org/?title={$title}&amp;url={$url}', 'cdata');
+			$this->setParam('e-param4', 'Partager sur Diaspora', 'cdata');
+			$this->setParam('e-param5', 'diaspora', 'cdata');
 
-			$this->saveParams();		
+		$this->saveParams();		
 		
-		}
+	}
 
 	
 	
 	/**
-	 * Méthode qui affiche les boutons sociaux 
-	 *
-	 * @return	stdio
-	 * @author	Stephane F, aruhuno, nIQnutn
-	 **/
+	* Méthode qui affiche les boutons des réseaux sociaux sur un article
+	*
+	* @author	Stephane F, aruhuno, nIQnutn
+	**/
 	public function MySocialNetworkArticle() {
-		
-		
 		
 		$plxShow = plxShow::getInstance(); // permet l'utilisation de plxShow
 		$onclick = "onclick=\"javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes');return false;\"";
 				
-
 		/* Récupération de l'URL */
 		$url = "?article".$plxShow->artId();
 		$url = $plxShow->plxMotor->urlRewrite($url);
-		//$test_url = str_replace("{\$url}", $url, $this->getParam("a-param3"));
-
 
 		/* Gestion du titre */
 		ob_start();
 		$plxShow->artTitle();
 		$title = rawurlencode(ob_get_clean());
-		//$test_title = str_replace("{\$title}", $title, $this->getParam("a-param3"));
 
-		
-		/* Remplace le motif {$url} et {$title} par l'url et le titre */	
+		/* Remplacement du motif {$url} et {$title} par l'URL et le titre de l'article */
 		$pattern = array("{\$url}","{\$title}");
 		$urlTitle   = array($url, $title);
 
 
+		echo "<div class=\"social-network\"><span class=\"share\">". $this->getLang('L_SHARE_ARTICLE')."</span><ul>";
 		
-		
-		
-		// Affiche les liens
-		echo "<div class=\"social-network\"><span class=\"share\">Partager cet article: </span><ul>";
-
 			if ($this->getParam('a-param2')=="1"){
 			echo "<li><a  title=\"".$this->getParam('a-param4')."\"  class=\"".$this->getParam('a-param5')."\" href=\"".str_replace($pattern, $urlTitle,  $this->getParam("a-param3"))."\" {$onclick} target=\"_blank\" rel=\"nofollow\" ><span>".$this->getParam('a-param4')."</span></a></li>";								
 			}
@@ -162,7 +148,12 @@ class plxMySocialNetwork extends plxPlugin {
 			if ($this->getParam('j-param2')=="1"){
 			echo "<li><a  title=\"".$this->getParam('j-param4')."\"  class=\"".$this->getParam('j-param5')."\" href=\"".str_replace($pattern, $urlTitle,  $this->getParam("j-param3"))."\" {$onclick} target=\"_blank\" rel=\"nofollow\" ><span>".$this->getParam('j-param4')."</span></a></li>";								
 			}
-			
+
+
+			if ($this->getParam('mail-param2')=="1"){
+			echo "<li><a  title=\"".$this->getParam('mail-param4')."\"  class=\"".$this->getParam('mail-param5')."\" href=\"".str_replace($pattern, $urlTitle,  $this->getParam("mail-param3"))."\"  target=\"_blank\" rel=\"nofollow\" ><span>".$this->getParam('mail-param4')."</span></a></li>";								
+			}			
+
 		echo "</ul></div>";			
 	}
 
@@ -170,11 +161,10 @@ class plxMySocialNetwork extends plxPlugin {
 
 
 	/**
-	 * Méthode qui affiche les boutons sociaux sur une page statique
-	 *
-	 * @return	stdio
-	 * @author	Stephane F, aruhuno, nIQnutn
-	 **/
+	* Méthode qui affiche les boutons sociaux sur une page statique
+	*
+	* @author	Stephane F, aruhuno, nIQnutn
+	**/
 	public function MySocialNetworkStatic() {
 				
 		$plxShow = plxShow::getInstance(); // permet l'utilisation de plxShow
@@ -189,13 +179,12 @@ class plxMySocialNetwork extends plxPlugin {
 		$plxShow->staticTitle();
 		$title = ob_get_clean();
 
-		/* Remplace le motif {$url} et {$title} par l'url et le titre */	
+		/* Remplacement du motif {$url} et {$title} par l'URL et le titre de la page statique */
 		$pattern = array("{\$url}","{\$title}");
 		$urlTitle   = array($url, $title);
 
 
-		// Affiche les liens
-		echo "<div class=\"social-network\"><span class=\"share\">Partager cette page: </span><ul>";
+		echo "<div class=\"social-network\"><span class=\"share\">". $this->getLang('L_SHARE_STATIC')."</span><ul>";
 
 			if ($this->getParam('a-param2')=="1"){
 			echo "<li><a  title=\"".$this->getParam('a-param4')."\"  class=\"".$this->getParam('a-param5')."\" href=\"".str_replace($pattern, $urlTitle,  $this->getParam("a-param3"))."\" {$onclick} target=\"_blank\" rel=\"nofollow\" ><span>".$this->getParam('a-param4')."</span></a></li>";								
@@ -236,7 +225,12 @@ class plxMySocialNetwork extends plxPlugin {
 			if ($this->getParam('j-param2')=="1"){
 			echo "<li><a  title=\"".$this->getParam('j-param4')."\"  class=\"".$this->getParam('j-param5')."\" href=\"".str_replace($pattern, $urlTitle,  $this->getParam("j-param3"))."\" {$onclick} target=\"_blank\" rel=\"nofollow\" ><span>".$this->getParam('j-param4')."</span></a></li>";								
 			}
-		
+
+
+			if ($this->getParam('mail-param2')=="1"){
+			echo "<li><a  title=\"".$this->getParam('mail-param4')."\"  class=\"".$this->getParam('mail-param5')."\" href=\"".str_replace($pattern, $urlTitle,  $this->getParam("mail-param3"))."\"  target=\"_blank\" rel=\"nofollow\" ><span>".$this->getParam('mail-param4')."</span></a></li>";								
+			}			
+	
 			
 		echo "</ul></div>";	
 	}
