f106d6df0141bb5e9b6a6e1f809152a855c0b7da
nIQnutn <sn@niqnutn.com>
2015-09-25 20:36:43 +0200

    Update fr.php

--- lang/fr.php
+++ lang/fr.php
@@ -14,6 +14,12 @@
 		'L_MAIL_SUBJECT'	=>	'Sujet',
 		'L_MAIL_BODY'		=>	'Corps du message',
 		'L_HELP'			=> 	'Aide',
+		'L_NB_ITEMS'		=> 	'Nombre maximum de liens',
+		'L_VALUE'			=> 	'Valeur',
+		'L_SAVE_CONFIG'		=>	'Enregistrer',
+		'L_LABEL_SHARE_ARTICLE'			=> 	'Libellé pour partager l\'article',
+		'L_LABEL_SHARE_STATIC'			=> 	'Libellé pour partager la page',
+		'L_LABEL_SHARE_LINKS'			=> 	'Libellé pour partager les liens',
 		
 # plxMySocialNetwork
 		'L_SHARE_ARTICLE'			=>	'Partager l\'article: ',
