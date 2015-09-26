109dd77b1af2a1b189005435bfc93fd330d14081
nIQnutn <sn@niqnutn.com>
2015-09-26 19:06:02 +0200

    simplification du hook

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 <?php
 
 // nb de liens max
-$maxItems= 10; 
+$maxItems= 12; 
 
 
 # Control du token du formulaire
@@ -28,13 +28,13 @@ if(!empty($_POST)) {
 	$plxPlugin->setParam('mail-param3', $_POST['mail-param3'], 'cdata');
 	$plxPlugin->setParam('mail-param4', $_POST['mail-param4'], 'cdata');
 	$plxPlugin->setParam('mail-param5', $_POST['mail-param5'], 'cdata');
+	$plxPlugin->setParam('mail-param8', $_POST['mail-param8'], 'cdata');
 	$plxPlugin->setParam('mail-param9', $_POST['mail-param9'], 'cdata');
 	$plxPlugin->setParam('mail-param0', $_POST['mail-param0'], 'cdata');	
 
 	$plxPlugin->setParam('maxItems', $_POST['maxItems'], 'numeric');
 	$plxPlugin->setParam('share-article', $_POST['share-article'], 'cdata');
 	$plxPlugin->setParam('share-page', $_POST['share-page'], 'cdata');
-	$plxPlugin->setParam('share-links', $_POST['share-links'], 'cdata');
 
 $plxPlugin->saveParams();
 header('Location: parametres_plugin.php?p=plxMySocialNetwork');
@@ -52,10 +52,10 @@ exit;
 		<thead>
 			<tr>
 				<th><?php  $plxPlugin->lang('L_NAME') ?></th>
-				<th><?php  $plxPlugin->lang('L_ACTIVE') ?></th>
-				<th><?php  $plxPlugin->lang('L_URL') ?></th>
-				<th><?php  $plxPlugin->lang('L_DESCRIPTION') ?></th>
-				<th><?php  $plxPlugin->lang('L_CLASS') ?></th>
+				<th><?php  $plxPlugin->lang('L_ACTIVE') ?> <a class="help" title="<?php  $plxPlugin->lang('L_ACTIVE_HELP') ?>">&nbsp;</a></th>
+				<th><?php  $plxPlugin->lang('L_URL') ?> <a class="help" title="<?php  $plxPlugin->lang('L_URL_HELP') ?>">&nbsp;</a></th>
+				<th><?php  $plxPlugin->lang('L_DESCRIPTION') ?> <a class="help" title="<?php  $plxPlugin->lang('L_DESCRIPTION_HELP') ?>">&nbsp;</a></th>
+				<th><?php  $plxPlugin->lang('L_CLASS') ?> <a class="help" title="<?php  $plxPlugin->lang('L_CLASS_HELP') ?>">&nbsp;</a></th>
 			</tr>
 		</thead>
 		<tbody>
@@ -76,57 +76,56 @@ exit;
 																					echo "<td>"; plxUtils::printSelect($i.'-param2',array('0'=>'Masquer','1'=>'Afficher'), $plxPlugin->getParam($i.'-param2')); echo "</td>";
 																					echo "<td>"; plxUtils::printInput($i.'-param3',$plxPlugin->getParam($i.'-param3'),'text','50-255') ; echo "</td>";
 																					echo "<td>"; plxUtils::printInput($i.'-param4',$plxPlugin->getParam($i.'-param4'),'text','35-100') ; echo "</td>";
-																					echo "<td>"; plxUtils::printInput($i.'-param5',$plxPlugin->getParam($i.'-param5'),'text','10-20') ; echo "</td>";
+																					echo "<td>"; plxUtils::printInput($i.'-param5','share-alt','text','10-20') ; echo "</td>";
 																	echo "</tr>" ;} 
 				} 
 				?>
 
-			<tr><td colspan="5"><div id="help">
-			<ul>
-			<li>Nom: nom du r&eacute;seau social</li>
-			<li>Active: activer/d&eacute;sactiver le widget</li>
-			<li>URL: url compl&egrave;te pour partager le l'article / page statique</li>
-			<li>Titre: description qui sera afficher (balise span  et infobulle)</li>
-			<li>class: nom de la classe utilis&eacute;e pour le CSS</li>
-			</ul>
-			<br />
-			<p>Remplacer l'<em>url</em> de la page par <strong>{$url}</strong> et son <em>titre</em> par <strong>{$title}</strong><br />
-					URL &agrave; indiqu&eacute;e dans le formulaire: <code>https://twitter.com/share?url={$url}&amp;text={$title}</code><br />
-				URL qui sera affich&eacute;e: <code>https://twitter.com/share?url=https://blog.niqnutn.com/index.php?article12&text=ArticleAPartager</code>
+			<tr><td colspan="4"><div id="help">
+
+			<p><?php  $plxPlugin->lang('L_PATTERN_URL') ?><br />
+			   <?php  $plxPlugin->lang('L_FORM_URL') ?>: <code>https://twitter.com/share?url={$url}&amp;text={$title}</code><br />
+			   <?php  $plxPlugin->lang('L_EXEMPLE_URL') ?>: <code>https://twitter.com/share?url=https://blog.niqnutn.com/index.php?article12&text=ArticleAPartager</code>
 			</p>
+		
 			</div></td></tr>
 		
+
 		</tbody>
 		
 	</table>
 	
 	
 	<table class="table">
-	<caption>Courriel</caption>
+	<caption><?php  $plxPlugin->lang('L_EMAIL') ?></caption>
 		<thead>
 			<tr>
 				<th><?php  $plxPlugin->lang('L_NAME') ?></th>
-				<th><?php  $plxPlugin->lang('L_ACTIVE') ?></th>
-				<th><?php  $plxPlugin->lang('L_URL') ?></th>
-				<th><?php  $plxPlugin->lang('L_DESCRIPTION') ?></th>
-				<th><?php  $plxPlugin->lang('L_CLASS') ?></th>
+				<th><?php  $plxPlugin->lang('L_ACTIVE') ?> <a class="help" title="<?php  $plxPlugin->lang('L_ACTIVE_HELP') ?>">&nbsp;</a></th>
+				<th><?php  $plxPlugin->lang('L_URL') ?> <a class="help" title="<?php  $plxPlugin->lang('L_URL_HELP') ?>">&nbsp;</a></th>
+				<th><?php  $plxPlugin->lang('L_DESCRIPTION') ?> <a class="help" title="<?php  $plxPlugin->lang('L_DESCRIPTION_HELP') ?>">&nbsp;</a></th>
+				<th><?php  $plxPlugin->lang('L_CLASS') ?> <a class="help" title="<?php  $plxPlugin->lang('L_CLASS_HELP') ?>">&nbsp;</a></th>
 			</tr>
 		</thead>	
 		<tbody>	
 			<tr>
 				<td><?php plxUtils::printInput('mail-param1',$plxPlugin->getParam('mail-param1'),'text','20-50') ?></td>
 				<td><?php plxUtils::printSelect('mail-param2',array('0'=>'Masquer','1'=>'Afficher'), $plxPlugin->getParam('mail-param2')) ?></td>
-				<td><?php plxUtils::printInput('mail-param3','mailto:?subject='.$plxPlugin->getParam('mail-param9').'&amp;body='.$plxPlugin->getParam('mail-param0'),'text','50-255', true)  ?></td>
+				<td><?php plxUtils::printInput('mail-param3','mailto:'.$plxPlugin->getParam('mail-param8').'?subject='.$plxPlugin->getParam('mail-param9').'&amp;body='.$plxPlugin->getParam('mail-param0'),'text','50-255', true)  ?></td>
 				<td><?php plxUtils::printInput('mail-param4',$plxPlugin->getParam('mail-param4'),'text','35-100') ?></td>
 				<td><?php plxUtils::printInput('mail-param5',$plxPlugin->getParam('mail-param5'),'text','10-20') ?></td>
 			</tr>
 			<tr>
-				<th ><?php  $plxPlugin->lang('L_MAIL_SUBJECT') ?></th>
+				<td ><?php  $plxPlugin->lang('L_EMAIL_SUBJECT') ?></td>
 				<td colspan="4"><?php plxUtils::printInput('mail-param9',$plxPlugin->getParam('mail-param9'),'text','80-255') ?></td>
 			</tr>
 			<tr>
-				<th ><?php  $plxPlugin->lang('L_MAIL_BODY') ?></th>
+				<td ><?php  $plxPlugin->lang('L_EMAIL_BODY') ?></td>
 				<td colspan="4"><?php plxUtils::printInput('mail-param0',$plxPlugin->getParam('mail-param0'),'text','80-255') ?></td>
+			</tr>
+			<tr>
+				<td ><?php  $plxPlugin->lang('L_EMAIL_RECIPIENT') ?></td>
+				<td colspan="4"><?php plxUtils::printInput('mail-param8',$plxPlugin->getParam('mail-param8'),'text','80-255') ?></td>
 			</tr>
 		</tbody>
 		</table>
@@ -153,10 +152,6 @@ exit;
 			<tr>
 				<td><?php $plxPlugin->lang('L_LABEL_SHARE_STATIC') ?></td>
 				<td><?php plxUtils::printInput('share-page',$plxPlugin->getParam('share-page'),'text','35-100') ?></td>
-			</tr>
-			<tr>
-				<td><?php $plxPlugin->lang('L_LABEL_SHARE_LINKS') ?></td>
-				<td><?php plxUtils::printInput('share-links',$plxPlugin->getParam('share-links'),'text','35-100') ?></td>
 			</tr>
 		</tbody>
 		</table>
